d8f9a84b5c491b4a767630389344d0216ed86a87
unknown <rayanbenmeddour@gmail.com>
2022-06-19 22:26:31 +0200

    wip

--- particles.js
+++ particles.js
@@ -20,10 +20,10 @@ var pJS = function(tag_id, params){
     },
     particles: {
       number: {
-        value: 400,
+        value: 25,
         density: {
           enable: true,
-          value_area: 800
+          value_area: 25
         }
       },
       color: {
